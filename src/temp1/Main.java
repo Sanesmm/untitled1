53495f14574b036179c930fb48eb64b5fbabfb37
Sanesmm <150282668+Sanesmm@users.noreply.github.com>
2023-11-27 10:07:50 +0100

    Update Main.java

--- src/temp1/Main.java
+++ src/temp1/Main.java
@@ -8,38 +8,37 @@ public class Main  {
     static Main r = new Main();  //här skapar jag objekt av klassen Main
 
     Scanner scan = new Scanner(System.in);  //för att läsa in från terminalen
-    String input = "";  //deklarera variabel string
+    static String input = "";  //deklarera variabel string
 
-    public void programIsRunning(String userInput){
+    public void programIsRunning(String userInput) {
 
-        while(!"stop".equalsIgnoreCase(input)) {   //här använder jag while loop som körs tills användaren skriver ordet stop
+        while (!"stop".equalsIgnoreCase(input)) {   //här använder jag while loop som körs tills användaren skriver ordet stop
 
             input = scan.nextLine();   //
-            if (!input.equalsIgnoreCase("stop")){
+            if (!input.equalsIgnoreCase("stop")) {
                 logic.antalTecken(userInput);
                 logic.antalRader();                  // här returner hur många rader finns i användarens input
-            }else{
+            } else {
                 System.out.println("Ordet stop är skriven!");
                 break;
             }
 
         }
-    }
 
+    }
     public static void main(String[] args) {
 
         System.out.println("Var god och skriv in en text: ");
         System.out.println("Skriv stop för att avsluta! ");
-        System.out.println("--------------------------------");
 
-        r.input = r.scan.next();   //sparar våra input
-        r.programIsRunning(r.input);
+        input = r.scan.next();     //sparar våra input
+        r.programIsRunning(input);
 
 
         System.out.println("--------------------------------------");
         System.out.println("Antal rader: " + logic.getAntalOrd());
         System.out.println("Antal tecken: " + logic.getAntalTecken());  //skriver ut information
-        System.out.println("Antal ord: " + logic.getAntalRader());
+        System.out.println("Antal ord: " + logic.getAntalRader(input.length()));
 
 
 
